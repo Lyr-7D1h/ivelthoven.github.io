5ab263704ea5281790ad3aa2c9d904f69cd4978f
ivelthoven <ivovelthoven@hotmail.com>
2019-05-08 16:47:56 +0200

    test

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const API_URL = "https://api.ivelthoven.nl/github";
 
 $(document).ready(function () {
+    console.log("js loaded..")
     $.getJSON( API_URL, function( data ) {
         $("#data").html(data);
         console.log(data);
